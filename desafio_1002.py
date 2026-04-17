f403840c36771aac9dcff873e7727f1ce3c48bc4
Lima2008 <Lucasbusiness387@gmail.com>
2026-04-17 11:45:31 -0300

    Fix typo in comment in desafio_1002.py
    Corrected a typo in a comment.

--- desafio_1002.py
+++ desafio_1002.py
@@ -17,4 +17,10 @@ exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla p
 
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1002
 
-# Escreva sua solução abaixo
+# Escreva sua soluçãov abaixo 
+
+raio = float(input())
+
+area = 3.14159 * raio ** 2
+
+print(f"A={area:.4f}")
